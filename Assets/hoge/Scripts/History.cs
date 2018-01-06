47406e440211240a099aae99eb9add8c60c84f25
sasann07151129 <sasann07151129@gmail.com>
2018-01-07 02:50:22 +0900

    来歴の追加

---
+++ Assets/hoge/Scripts/History.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class History {
+    public int turningPoint { set; get; }
+    public string location { set; get; }
+    public string somebody { set; get; }
+    public string something { set; get; }
+    public string action { set; get; }
+}
